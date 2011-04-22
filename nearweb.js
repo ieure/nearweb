5976d5ff13a61b99d8e5a427593d67926f68a84b
Ian Eure <ian.eure@gmail.com>
2011-04-21 17:42:47 -0700

    Bug fixes, save state etc.

--- nearweb.js
+++ nearweb.js
@@ -8,6 +8,7 @@ $(function() {
             return;
         }
         $('#status').hide();
+        $('li.place').remove();
         for (var i in places['features']) {
             if (!places['features'][i].properties['website']) {
                 continue;
@@ -17,7 +18,23 @@ $(function() {
                     places['features'][i].properties['website'] + "\">" +
                     places['features'][i].properties['name'] + "</a></li>");
         };
+        $('#refresh').removeClass('disabled').show();
     };
+
+    var tries = 0;
+
+    var updateLocation = function(lat, lon) {
+        window.title = "Nearweb: Places near " + lat + "," + lon;
+        window.history.pushState(undefined, window.title,
+                                 window.location.pathname + '#' + lat +
+                                 ',' + lon);
+    };
+
+    var placesSearch = function(lat, lon) {
+        updateLocation(lat, lon);
+        places.search(lat, lon, {}, populatePlaces);
+    };
+
 
     var placesSearchThunk = function(err, position) {
         if (err) {
@@ -26,9 +43,34 @@ $(function() {
             return;
         }
         console.log(position);
-        places.search(position.coords.latitude, position.coords.longitude, {},
-                      populatePlaces);
+
+        if (position.coords.accuracy >= 150 && tries <= 3) {
+            tries += 1;
+            $('#status').show().html("Stop moving!");
+            load();
+        }
+        placesSearch(position.coords.latitude, position.coords.longitude);
     };
 
-    client.getLocationFromBrowser({}, placesSearchThunk);
+    var load = function() {
+        client.getLocationFromBrowser({enableHighAccuracy: true,
+                                       maximumAge: 0},
+                                      placesSearchThunk);
+    };
+
+    $('#refresh').click(function(event) {
+        if ($(this).is('.disabled')) {
+            return;
+        }
+        $(this).addClass('disabled').hide();
+        $('#status').html("Loading…").show();
+        load();
+    });
+
+    if (window.location.hash != "") {
+        var coords = window.location.hash.substring(1).split(",");
+        placesSearch(coords[0], coords[1]);
+    } else {
+        load();
+    }
 });
